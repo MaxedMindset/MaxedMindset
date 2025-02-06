44cbc4e2fb57d562a49a33a95b73c51303d7bc39
Max <127038155+MaxedMindset@users.noreply.github.com>
2025-02-06 19:44:38 +0100

    Create AppDelegate.swift

---
+++ AppDelegate.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    // Standard‑Implementierung
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
